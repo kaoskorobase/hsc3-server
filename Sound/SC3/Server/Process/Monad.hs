a1bd972735a6f953c289a4eb595652978d5f71ce
Stefan Kersten <sk@xdv.org>
2011-04-14 12:27:23 +0200

    Add server context functions with default options
    Also re-export option and output handler types and functions for
    convenience.

--- Sound/SC3/Server/Process/Monad.hs
+++ Sound/SC3/Server/Process/Monad.hs
@@ -1,16 +1,23 @@
 module Sound.SC3.Server.Process.Monad (
     withSynth
+  , withDefaultSynth
   , withInternal
+  , withDefaultInternal
+  -- * Re-exported for convenience
+  , module Sound.SC3.Server.Options
+  , OutputHandler(..)
+  , defaultOutputHandler
 ) where
 
-import           Sound.SC3.Server.Process (ServerOptions, OutputHandler, RTOptions)
-import qualified Sound.SC3.Server.Process as Process
+import qualified Sound.OpenSoundControl as OSC
 import qualified Sound.SC3.Server.Connection as Conn
-import qualified Sound.SC3.Server.State as State
 import qualified Sound.SC3.Server.Internal as Process
 import           Sound.SC3.Server.Monad (Server)
 import qualified Sound.SC3.Server.Monad as Server
-import qualified Sound.OpenSoundControl as OSC
+import           Sound.SC3.Server.Options
+import           Sound.SC3.Server.Process ( OutputHandler(..), defaultOutputHandler )
+import qualified Sound.SC3.Server.Process as Process
+import qualified Sound.SC3.Server.State as State
 
 withSynth :: OSC.Transport t => (ServerOptions -> RTOptions -> IO t) -> ServerOptions -> RTOptions -> OutputHandler -> Server a -> IO a
 withSynth openTransport serverOptions rtOptions outputHandler action =
@@ -21,6 +28,9 @@ withSynth openTransport serverOptions rtOptions outputHandler action =
         outputHandler
         $ \t -> Conn.new (State.new serverOptions) t >>= Server.runServer action
 
+withDefaultSynth :: OSC.Transport t => (ServerOptions -> RTOptions -> IO t) -> Server a -> IO a
+withDefaultSynth openTransport action = withSynth openTransport defaultServerOptions defaultRTOptions defaultOutputHandler action
+
 withInternal :: ServerOptions -> RTOptions -> OutputHandler -> Server a -> IO a
 withInternal serverOptions rtOptions outputHandler action =
     Process.withInternal
@@ -28,3 +38,6 @@ withInternal serverOptions rtOptions outputHandler action =
         rtOptions
         outputHandler
         $ \t -> Conn.new (State.new serverOptions) t >>= Server.runServer action
+
+withDefaultInternal :: Server a -> IO a
+withDefaultInternal = withInternal defaultServerOptions defaultRTOptions defaultOutputHandler
