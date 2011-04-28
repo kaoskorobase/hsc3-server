aa2e982a4b04757d717923c33650a28299292630
Stefan Kersten <sk@xdv.org>
2011-04-28 12:09:28 +0200

    Factor allocation, OSC send and receive monad interface into typeclasses
    This makes it easier to restrict monadic actions to certain effects and facilitates lifting operations in a monad transformer stack.

--- Sound/SC3/Server/Monad.hs
+++ Sound/SC3/Server/Monad.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE FlexibleContexts
+           , GeneralizedNewtypeDeriving #-}
 module Sound.SC3.Server.Monad
   ( -- * Server Monad
     ServerT
@@ -8,7 +9,6 @@ module Sound.SC3.Server.Monad
   , liftIO
   , connection
   , serverOptions
-  , rootNodeId
   -- * Allocation
   , Allocator
   , BufferId
@@ -21,37 +21,30 @@ module Sound.SC3.Server.Monad
   , NodeId
   , NodeIdAllocator
   , nodeIdAllocator
-  , alloc
-  , free
-  , allocMany
-  , freeMany
   , Range
-  , allocRange
-  , freeRange
-  -- * Concurrency
-  , fork
+  , MonadIdAllocator(..)
   -- * Communication and synchronization
+  , MonadSendOSC(..)
+  , MonadRecvOSC(..)
   , SyncId
   , SyncIdAllocator
   , syncIdAllocator
-  , send
-  , waitFor
-  , waitFor_
-  , waitForAll
-  , waitForAll_
   , sync
   , unsafeSync
+  -- * Concurrency
+  , fork
   ) where
 
 import           Control.Applicative
 import           Control.Concurrent (ThreadId, forkIO)
 import           Control.Concurrent.MVar.Strict
+import           Control.Failure (Failure)
 import           Control.Monad.IO.Class (MonadIO, liftIO)
 import           Control.Monad.Trans.Reader (ReaderT(..), ask, asks)
 import           Control.Monad.Trans.Class (MonadTrans)
 import           Data.Accessor
 import           Sound.OpenSoundControl (OSC)
-import           Sound.SC3.Server.Allocator (Id, IdAllocator, RangeAllocator, Range)
+import           Sound.SC3.Server.Allocator (AllocFailure, Id, IdAllocator, RangeAllocator, Range)
 import           Sound.SC3.Server.Connection (Connection)
 import qualified Sound.SC3.Server.Connection as C
 import           Sound.SC3.Server.Notification (Notification)
@@ -91,55 +84,67 @@ connection = liftConn return
 serverOptions :: MonadIO m => ServerT m ServerOptions
 serverOptions = liftState (getVal State.serverOptions)
 
--- | Return the root node id.
-rootNodeId :: MonadIO m => ServerT m NodeId
-rootNodeId = liftState State.rootNodeId
+-- | Server resource id management interface.
+class Monad m => MonadIdAllocator m where
+    -- | Return the root node id.
+    rootNodeId :: m NodeId
 
--- | Allocate an id using the given allocator.
-alloc :: (IdAllocator a, MonadIO m) => Allocator a -> ServerT m (Id a)
-alloc a = liftConn $ \c -> C.alloc c a
+    -- | Allocate an id using the given allocator.
+    alloc :: (IdAllocator a) => Allocator a -> m (Id a)
 
--- | Free an id using the given allocator.
-free :: (IdAllocator a, MonadIO m) => Allocator a -> Id a -> ServerT m ()
-free a i = liftConn $ \c -> C.free c a i
+    -- | Free an id using the given allocator.
+    free :: (IdAllocator a) => Allocator a -> Id a -> m ()
 
--- | Allocate a number of ids using the given allocator.
-allocMany :: (IdAllocator a, MonadIO m) => Allocator a -> Int -> ServerT m [Id a]
-allocMany a n = liftConn $ \c -> C.allocMany c a n
+    -- | Allocate a number of ids using the given allocator.
+    allocMany :: (IdAllocator a) => Allocator a -> Int -> m [Id a]
 
--- | Free a number of ids using the given allocator.
-freeMany :: (IdAllocator a, MonadIO m) => Allocator a -> [Id a] -> ServerT m ()
-freeMany a is = liftConn $ \c -> C.freeMany c a is
+    -- | Free a number of ids using the given allocator.
+    freeMany :: (IdAllocator a) => Allocator a -> [Id a] -> m ()
 
--- | Allocate a contiguous range of ids using the given allocator.
-allocRange :: (RangeAllocator a, MonadIO m) => Allocator a -> Int -> ServerT m (Range (Id a))
-allocRange a n = liftConn $ \c -> C.allocRange c a n
+    -- | Allocate a contiguous range of ids using the given allocator.
+    allocRange :: (RangeAllocator a) => Allocator a -> Int -> m (Range (Id a))
 
--- | Free a contiguous range of ids using the given allocator.
-freeRange :: (RangeAllocator a, MonadIO m) => Allocator a -> Range (Id a) -> ServerT m ()
-freeRange a r = liftConn $ \c -> C.freeRange c a r
+    -- | Free a contiguous range of ids using the given allocator.
+    freeRange :: (RangeAllocator a) => Allocator a -> Range (Id a) -> m ()
 
--- | Fork a computation in a new thread and return the thread id.
-fork :: (MonadIO m) => ServerT IO () -> ServerT m ThreadId
-fork a = liftConn $ \c -> forkIO (runServerT a c)
 
-send :: (MonadIO m) => OSC -> ServerT m ()
-send osc = liftConn $ \c -> C.send c osc
+instance MonadIO m => MonadIdAllocator (ServerT m) where
+    rootNodeId = liftState State.rootNodeId
+    alloc a = liftConn $ \c -> C.alloc c a
+    free a i = liftConn $ \c -> C.free c a i
+    allocMany a n = liftConn $ \c -> C.allocMany c a n
+    freeMany a is = liftConn $ \c -> C.freeMany c a is
+    allocRange a n = liftConn $ \c -> C.allocRange c a n
+    freeRange a r = liftConn $ \c -> C.freeRange c a r
 
-waitFor :: (MonadIO m) => OSC -> Notification a -> ServerT m a
-waitFor osc n = liftConn $ \c -> C.waitFor c osc n
+class Monad m => MonadSendOSC m where
+    send :: OSC -> m ()
 
-waitFor_ :: (MonadIO m) => OSC -> Notification a -> ServerT m ()
-waitFor_ osc n = liftConn $ \c -> C.waitFor_ c osc n
+instance MonadIO m => MonadSendOSC (ServerT m) where
+    send osc = liftConn $ \c -> C.send c osc
 
-waitForAll :: (MonadIO m) => OSC -> [Notification a] -> ServerT m [a]
-waitForAll osc ns = liftConn $ \c -> C.waitForAll c osc ns
+class Monad m => MonadRecvOSC m where
+    -- | Wait for a notification and return the result.
+    waitFor :: OSC -> Notification a -> m a
+    -- | Wait for a notification and ignore the result.
+    waitFor_ :: OSC -> Notification a -> m ()
+    -- | Wait for a set of notifications and return their results in unspecified order.
+    waitForAll :: OSC -> [Notification a] -> m [a]
+    -- | Wait for a set of notifications and ignore their results.
+    waitForAll_ :: OSC -> [Notification a] -> m ()
 
-waitForAll_ :: (MonadIO m) => OSC -> [Notification a] -> ServerT m ()
-waitForAll_ osc ns = liftConn $ \c -> C.waitForAll_ c osc ns
+instance MonadIO m => MonadRecvOSC (ServerT m) where
+    waitFor osc n = liftConn $ \c -> C.waitFor c osc n
+    waitFor_ osc n = liftConn $ \c -> C.waitFor_ c osc n
+    waitForAll osc ns = liftConn $ \c -> C.waitForAll c osc ns
+    waitForAll_ osc ns = liftConn $ \c -> C.waitForAll_ c osc ns
 
 sync :: (MonadIO m) => OSC -> ServerT m ()
 sync osc = liftConn $ \c -> C.sync c osc
 
 unsafeSync :: (MonadIO m) => ServerT m ()
 unsafeSync = liftConn C.unsafeSync
+
+-- | Fork a computation in a new thread and return the thread id.
+fork :: (MonadIO m) => ServerT IO () -> ServerT m ThreadId
+fork a = liftConn $ \c -> forkIO (runServerT a c)
