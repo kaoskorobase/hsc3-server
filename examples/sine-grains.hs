2b582136150f5e67d059da8acaf4f26c3f17e74d
Kaoskorobase <kaoskorobase@gmail.com>
2012-04-09 20:05:11 +0200

    Add sine grain example

---
+++ examples/sine-grains.hs
@@ -0,0 +1,49 @@
+import           Control.Applicative
+import           Control.Concurrent
+import           Control.Monad
+import           Control.Monad.IO.Class (MonadIO(..))
+import           Sound.SC3.Server.Process.Monad
+import           Sound.SC3.UGen
+import           Sound.SC3.Server.Monad
+import           Sound.SC3.Server.Monad.Command
+import           Sound.SC3.Server.Monad.Send ((~>), (!>), async)
+import           Sound.SC3.Server.Notification
+import           Sound.OpenSoundControl (immediately)
+import qualified Sound.OpenSoundControl as OSC
+import           System.Random
+
+sine = out 0 $ pan2 x (sinOsc KR 1 0 * 0.6) 1
+    where x = sinOsc AR (control KR "freq" 440) 0
+                * control KR "amp" 1
+                * envGen KR (control KR "gate" 1) 1 0 1 RemoveSynth (envASR 0.02 1 0.1 EnvLin)
+
+pauseThread = liftIO . OSC.pauseThread
+pauseThreadUntil = liftIO . OSC.pauseThreadUntil
+
+statusLoop = do
+    immediately !> status >>= liftIO . print
+    pauseThread 1
+    statusLoop
+
+grainLoop sd delta sustain t = do
+    f <- liftIO $ randomRIO (100,800)
+    a <- liftIO $ randomRIO (0,0.5)
+    r <- rootNode
+    synth <- OSC.UTCr (t + latency) ~> s_new sd AddToTail r [("freq", f), ("amp", a)]
+    fork $ do
+        let t' = t + sustain
+        pauseThreadUntil t'
+        OSC.UTCr (t' + latency) ~> s_release (-1) synth
+    let t' = t + delta
+    pauseThreadUntil t'
+    grainLoop sd delta sustain t'
+
+--run = withDefaultSynth
+run = withDefaultInternal
+
+latency = 0.03
+ 
+main = run $ do
+    sd <- immediately !> async (d_new "hsc3-server:sine" sine)
+    fork statusLoop
+    grainLoop sd 0.03 0.06 =<< liftIO OSC.utcr
