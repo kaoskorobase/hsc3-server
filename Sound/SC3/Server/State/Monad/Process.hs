5f6a8fc54e5482c33a73b70a163d2714b48dfcb5
Stefan Kersten <kaoskorobase@gmail.com>
2012-11-22 00:07:40 +0100

    Fix indentation

--- Sound/SC3/Server/State/Monad/Process.hs
+++ Sound/SC3/Server/State/Monad/Process.hs
@@ -1,8 +1,8 @@
 module Sound.SC3.Server.State.Monad.Process (
-    withSynth
-  , withDefaultSynth
-  -- * Re-exported for convenience
-  , module Sound.SC3.Server.Process
+  withSynth
+, withDefaultSynth
+-- * Re-exported for convenience
+, module Sound.SC3.Server.Process
 ) where
 
 import           Data.Default (def)
@@ -14,11 +14,11 @@ import qualified Sound.SC3.Server.State.Monad as Server
 
 withSynth :: ServerOptions -> RTOptions -> OutputHandler -> Server a -> IO a
 withSynth serverOptions rtOptions outputHandler action =
-    Process.withSynth
-        serverOptions
-        rtOptions
-        outputHandler
-        $ \t -> Conn.open t >>= Server.runServer action serverOptions
+  Process.withSynth
+    serverOptions
+    rtOptions
+    outputHandler
+    $ \t -> Conn.open t >>= Server.runServer action serverOptions
 
 withDefaultSynth :: Server a -> IO a
 withDefaultSynth = withSynth def def def
