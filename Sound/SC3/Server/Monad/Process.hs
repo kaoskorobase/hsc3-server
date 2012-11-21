7c690009a716267f7213b019f06769997fe5b478
Stefan Kersten <kaoskorobase@gmail.com>
2012-11-21 17:32:32 +0100

    Use Data.Default and export Sound.SC3.Server.Process

--- Sound/SC3/Server/Monad/Process.hs
+++ Sound/SC3/Server/Monad/Process.hs
@@ -2,17 +2,15 @@ module Sound.SC3.Server.Monad.Process (
     withSynth
   , withDefaultSynth
   -- * Re-exported for convenience
-  , module Sound.SC3.Server.Process.Options
-  , OutputHandler(..)
-  , defaultOutputHandler
+  , module Sound.SC3.Server.Process
 ) where
 
+import           Data.Default (def)
 import qualified Sound.SC3.Server.Connection as Conn
 import           Sound.SC3.Server.Monad (Server)
 import qualified Sound.SC3.Server.Monad as Server
-import           Sound.SC3.Server.Process (OutputHandler(..), defaultOutputHandler)
+import           Sound.SC3.Server.Process hiding (withSynth)
 import qualified Sound.SC3.Server.Process as Process
-import           Sound.SC3.Server.Process.Options
 
 withSynth :: ServerOptions -> RTOptions -> OutputHandler -> Server a -> IO a
 withSynth serverOptions rtOptions outputHandler action =
@@ -23,4 +21,4 @@ withSynth serverOptions rtOptions outputHandler action =
         $ \t -> Conn.open t >>= Server.runServer action serverOptions
 
 withDefaultSynth :: Server a -> IO a
-withDefaultSynth = withSynth defaultServerOptions defaultRTOptions defaultOutputHandler
+withDefaultSynth = withSynth def def def
