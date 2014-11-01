602ba10e5cc20e73bc6782072491c100650ec776
Stefan Kersten <stefan@samplecount.com>
2014-11-01 14:09:16 +0100

    Use fromNonNegative instead of fromIntegral
    The code with `fromIntegral` doesn't compile with QuickCheck-2.7.1 on Travis.

--- tests/Sound/SC3/Server/Allocator/Range/Test.hs
+++ tests/Sound/SC3/Server/Allocator/Range/Test.hs
@@ -10,7 +10,7 @@ import Prelude hiding (null)
 import Control.Applicative ((<$>), (<*>))
 import Test.Framework (Test, testGroup)
 import Test.Framework.Providers.QuickCheck2 (testProperty)
-import Test.QuickCheck (Arbitrary(..), NonNegative)
+import Test.QuickCheck (Arbitrary(..), NonNegative, getNonNegative)
 
 instance (Integral i, Arbitrary i) => Arbitrary (Range i) where
     arbitrary = sized <$> ((`mod`10000) <$> arbitrary) <*> arbitrary
@@ -20,11 +20,12 @@ tests =
     [ testGroup "Sound.SC3.Server.Allocator.Range"
         [ testProperty "bounds" $ \(r :: Range Int) -> begin r <= end r
         , testProperty "split" $ \(n :: NonNegative Int) (r :: Range Int) ->
-            let n' = fromIntegral n
+            let n' = getNonNegative n
                 (r1, r2) = split n' r in
             if n' > size r
                 then r1 == r && null r2
                 else size r1 == n' && size r2 == (size r - n')
-        , testProperty "split/join" $ \(n :: Int) (r :: Range Int) -> uncurry join (split n r) == r
+        , testProperty "split/join" $ \(n :: Int) (r :: Range Int) ->
+            uncurry join (split n r) == r
         ]
     ]
